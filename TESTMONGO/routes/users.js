bf5172b236fbc41a046c6cb56c6b31b652afefd0
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-11-06 11:03:37 +0000

    esercizio fatto :) <3

--- TESTMONGO/routes/users.js
+++ TESTMONGO/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(res);
+router.get('/', function (req, res, next) {
+    const uri = "mongodb+srv://andrea-favullo:m0ng0D4RI0B4nF1@cluster0.dnkc2.mongodb.net/Cluster0?retryWrites=true&w=majority"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // perform actions on the collection object
+        collection.find().limit(10).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+    //res.send(result);
 });
 
 module.exports = router;
