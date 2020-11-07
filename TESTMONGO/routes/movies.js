c4623cdff004c52861469eacd8e808ea81f4d626
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-11-07 23:07:55 +0000

    aggiunta lista film

--- TESTMONGO/routes/movies.js
+++ TESTMONGO/routes/movies.js
@@ -1,22 +1,77 @@
 var express = require('express');
 var router = express.Router();
 
-const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
-//
+//Importo la libreria mongodb
+const MongoClient = require('mongodb').MongoClient;
+
+//ricerca di un film usand il titolo
 router.get('/movie_from_title/:title', function (req, res, next) {
-    console.log(req.params); //Leggo i parametri passati all'url
+    //Leggo i parametri passati all'url
+    console.log(req.params);
     title = req.params.title;
+    //stringa di connessione al db
     const uri = "mongodb+srv://andrea-favullo:m0ng0D4RI0B4nF1@cluster0.dnkc2.mongodb.net/Cluster0?retryWrites=true&w=majority"
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
-        // eseguo una find sulla collection
-        collection.find({ 'title': `${title}` }).toArray((err, result) => {
-            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-            else res.send(result);
-            client.close(); //Quando ho terminato la find chiudo la sessione con il db
-        }); //Eseguo la query e passo una funzione di callback
 
+    client.connect(err => {
+        // mi connetto alla collection movies
+        const collection = client.db("sample_mflix").collection("movies");
+        // trovo il film nella collection
+        collection.find({ 'title': `${title}` }).toArray((err, result) => {
+            //Se c'è qualche errore lo stampo
+            if (err) console.log(err.message);
+            else res.send(result);
+            //Quando ho terminato la find chiudo la sessione con il db
+            client.close();
+        }); 
+        //Eseguo la query e passo una funzione di callback
+    });
+});
+
+//ricerca di un film usand il titolo
+router.get('/list/:num', function (req, res, next) {
+    //Leggo i parametri passati all'url
+    console.log(req.params);
+    let num = parseInt(req.params.num);
+    //stringa di connessione al db
+    const uri = "mongodb+srv://andrea-favullo:m0ng0D4RI0B4nF1@cluster0.dnkc2.mongodb.net/Cluster0?retryWrites=true&w=majority"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    client.connect(err => {
+        // mi connetto alla collection movies
+        const collection = client.db("sample_mflix").collection("movies");
+        // prendo num film dalla collection
+        if (num>0){
+            collection.find().limit(num).toArray((err, result) => {
+                //Se c'è qualche errore lo stampo
+                if (err) console.log(err.message);
+                else res.send(result);
+                //Quando ho terminato la find chiudo la sessione con il db
+                client.close();
+            });
+        }else{
+            res.send("0 film restituiti")
+        }
+        //Eseguo la query e passo una funzione di callback
+    });
+});
+router.get('/list/', function (req, res, next) {
+    //stringa di connessione al db
+    const uri = "mongodb+srv://andrea-favullo:m0ng0D4RI0B4nF1@cluster0.dnkc2.mongodb.net/Cluster0?retryWrites=true&w=majority"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    client.connect(err => {
+        // mi connetto alla collection movies
+        const collection = client.db("sample_mflix").collection("movies");
+        // prendo num film dalla collection
+        collection.find().limit(10).toArray((err, result) => {
+            //Se c'è qualche errore lo stampo
+            if (err) console.log(err.message);
+            else res.send(result);
+            //Quando ho terminato la find chiudo la sessione con il db
+            client.close();
+        });
+        //Eseguo la query e passo una funzione di callback
     });
 });
 
