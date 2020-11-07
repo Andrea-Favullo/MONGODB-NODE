56534abe7a53d45e8b158a1570360b668736ba28
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-11-07 23:26:42 +0000

    ricerca in base alla valutazione

--- TESTMONGO/routes/movies.js
+++ TESTMONGO/routes/movies.js
@@ -80,7 +80,6 @@ router.get('/year/:year', function (req, res, next) {
     //Leggo i parametri passati all'url
     console.log(req.params);
     let year = parseInt(req.params.year);
-    console.log(year);
     //stringa di connessione al db
     const uri = "mongodb+srv://andrea-favullo:m0ng0D4RI0B4nF1@cluster0.dnkc2.mongodb.net/Cluster0?retryWrites=true&w=majority"
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -90,6 +89,30 @@ router.get('/year/:year', function (req, res, next) {
         const collection = client.db("sample_mflix").collection("movies");
         // prendo num film dalla collection
         collection.find({ 'year': year }).toArray((err, result) => {
+            //Se c'è qualche errore lo stampo
+            if (err) console.log(err.message);
+            else res.send(result);
+            //Quando ho terminato la find chiudo la sessione con il db
+            client.close();
+        });
+        //Eseguo la query e passo una funzione di callback
+    });
+});
+
+//ricerca di un film in base alla valutazione
+router.get('/rating/:rate', function (req, res, next) {
+    //Leggo i parametri passati all'url
+    console.log(req.params);
+    let rate = parseInt(req.params.rate);
+    //stringa di connessione al db
+    const uri = "mongodb+srv://andrea-favullo:m0ng0D4RI0B4nF1@cluster0.dnkc2.mongodb.net/Cluster0?retryWrites=true&w=majority"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    client.connect(err => {
+        // mi connetto alla collection movies
+        const collection = client.db("sample_mflix").collection("movies");
+        // prendo num film dalla collection
+        collection.find({ 'metacritic': rate }).toArray((err, result) => {
             //Se c'è qualche errore lo stampo
             if (err) console.log(err.message);
             else res.send(result);
